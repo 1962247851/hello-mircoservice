a78b81792ae063e26bfa49e40e5a61450339b244
Miao JinZhou <1962247851@qq.com>
2021-10-21 11:44:16 +0800

    feat(upms): 增加图片上传后自动保存压缩后副本功能

--- ordinaryroad/ordinaryroad-upms/ordinaryroad-upms-biz/src/main/java/tech/ordinaryroad/ordinaryroad/admin/service/impl/SysFileServiceImpl.java
+++ ordinaryroad/ordinaryroad-upms/ordinaryroad-upms-biz/src/main/java/tech/ordinaryroad/ordinaryroad/admin/service/impl/SysFileServiceImpl.java
@@ -22,20 +22,26 @@ import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
 import com.amazonaws.services.s3.model.S3Object;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import tech.ordinaryroad.ordinaryroad.admin.api.entity.SysFile;
-import tech.ordinaryroad.ordinaryroad.admin.mapper.SysFileMapper;
-import tech.ordinaryroad.ordinaryroad.admin.service.SysFileService;
-import tech.ordinaryroad.ordinaryroad.common.core.util.R;
 import com.pig4cloud.plugin.oss.OssProperties;
 import com.pig4cloud.plugin.oss.service.OssTemplate;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.tasks.UnsupportedFormatException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import tech.ordinaryroad.ordinaryroad.admin.api.entity.SysFile;
+import tech.ordinaryroad.ordinaryroad.admin.mapper.SysFileMapper;
+import tech.ordinaryroad.ordinaryroad.admin.service.SysFileService;
+import tech.ordinaryroad.ordinaryroad.common.core.util.R;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -56,31 +62,64 @@ public class SysFileServiceImpl extends ServiceImpl<SysFileMapper, SysFile> impl
 
 	/**
 	 * 上传文件
+	 *
 	 * @param file
 	 * @return
 	 */
 	@Override
 	public R uploadFile(MultipartFile file) {
-		String fileName = IdUtil.simpleUUID() + StrUtil.DOT + FileUtil.extName(file.getOriginalFilename());
+		String defaultBucketName = ossProperties.getBucketName();
+		String fileOriginalFilename = file.getOriginalFilename();
+
+		String fileName = IdUtil.simpleUUID() + StrUtil.DOT + FileUtil.extName(fileOriginalFilename);
 		Map<String, String> resultMap = new HashMap<>(4);
-		resultMap.put("bucketName", ossProperties.getBucketName());
+		resultMap.put("bucketName", defaultBucketName);
 		resultMap.put("fileName", fileName);
-		resultMap.put("url", String.format("/admin/sys-file/%s/%s", ossProperties.getBucketName(), fileName));
+		resultMap.put("url", String.format("/admin/sys-file/%s/%s", defaultBucketName, fileName));
 
 		try {
-			minioTemplate.putObject(ossProperties.getBucketName(), fileName, file.getInputStream());
+			minioTemplate.putObject(defaultBucketName, fileName, file.getInputStream());
+			// 如果是图片，保存缩略图
+			saveThumbnailIfImage(fileName, file);
 			// 文件管理数据记录,收集管理追踪文件
-			fileLog(file, fileName);
-		}
-		catch (Exception e) {
+			fileLog(defaultBucketName, fileOriginalFilename, file.getSize(), fileName);
+		} catch (Exception e) {
 			log.error("上传失败", e);
 			return R.failed(e.getLocalizedMessage());
 		}
 		return R.ok(resultMap);
 	}
 
+	private void saveThumbnailIfImage(String fileName, MultipartFile file) throws IOException {
+		String thumbnailBucketName = "thumbnail";
+		boolean isPicture = true;
+		// 缩略图
+		InputStream inputStream = file.getInputStream();
+		try {
+			File outFile = FileUtil.createTempFile(FileUtil.extName(fileName), StrUtil.DOT + FileUtil.getSuffix(fileName), FileUtil.getTmpDir(), false);
+			Thumbnails.of(inputStream).scale(1.0f).outputQuality(0.5f).toFile(outFile);
+
+			minioTemplate.createBucket(thumbnailBucketName);
+			minioTemplate.putObject(thumbnailBucketName, fileName, new FileInputStream(outFile));
+			// 文件管理数据记录,收集管理追踪文件
+			fileLog(thumbnailBucketName, file.getOriginalFilename(), outFile.length(), fileName);
+		} catch (IOException e) {
+			// 不是图片不用管，isPicture = false
+			if (e instanceof UnsupportedFormatException) {
+				isPicture = false;
+			} else {
+				e.printStackTrace();
+			}
+			// 图片报错
+			if (isPicture) {
+				log.error("图片压缩异常: {}", e.getLocalizedMessage());
+			}
+		}
+	}
+
 	/**
 	 * 读取文件
+	 *
 	 * @param bucket
 	 * @param fileName
 	 * @param response
@@ -90,14 +129,14 @@ public class SysFileServiceImpl extends ServiceImpl<SysFileMapper, SysFile> impl
 		try (S3Object s3Object = minioTemplate.getObject(bucket, fileName)) {
 			response.setContentType("application/octet-stream; charset=UTF-8");
 			IoUtil.copy(s3Object.getObjectContent(), response.getOutputStream());
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			log.error("文件读取异常: {}", e.getLocalizedMessage());
 		}
 	}
 
 	/**
 	 * 删除文件
+	 *
 	 * @param id
 	 * @return
 	 */
@@ -106,22 +145,25 @@ public class SysFileServiceImpl extends ServiceImpl<SysFileMapper, SysFile> impl
 	@Transactional(rollbackFor = Exception.class)
 	public Boolean deleteFile(Long id) {
 		SysFile file = this.getById(id);
-		minioTemplate.removeObject(ossProperties.getBucketName(), file.getFileName());
+		minioTemplate.removeObject(file.getBucketName(), file.getFileName());
 		return this.removeById(id);
 	}
 
 	/**
 	 * 文件管理数据记录,收集管理追踪文件
-	 * @param file 上传文件格式
+	 *
+	 * @param bucketName 桶名称
+	 * @param originalFileName 原始文件名
+	 * @param size 文件大小
 	 * @param fileName 文件名
 	 */
-	private void fileLog(MultipartFile file, String fileName) {
+	private void fileLog(String bucketName, String originalFileName, Long size, String fileName) {
 		SysFile sysFile = new SysFile();
 		sysFile.setFileName(fileName);
-		sysFile.setOriginal(file.getOriginalFilename());
-		sysFile.setFileSize(file.getSize());
-		sysFile.setType(FileUtil.extName(file.getOriginalFilename()));
-		sysFile.setBucketName(ossProperties.getBucketName());
+		sysFile.setOriginal(originalFileName);
+		sysFile.setFileSize(size);
+		sysFile.setType(FileUtil.extName(originalFileName));
+		sysFile.setBucketName(bucketName);
 		this.save(sysFile);
 	}
 
